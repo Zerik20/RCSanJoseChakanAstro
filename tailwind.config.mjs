161616e0ac93a4b4ba90d3c92519cded82e23f63
Zerik20 <erikaban14@gmail.com>
2024-09-13 10:39:19 -0600

    Se termino de agregar la documentacion y las redes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#9a1b00',   // Rojo 
+				secondary: '#F97316', // Naranja
+				accent: '#10B981',    // Verde
+				neutral: '#F3F4F6',   // Gris claro
+				dark: '#1F2937'       // Gris oscuro
+			},
+		},
 	},
 	plugins: [
 		require('flowbite/plugin')
